54381d10e2fa406ce1a504c1d820b44c666aaf57
Md Asikuzzaman <mdasikuzzaman.en@gmail.com>
2025-04-29 17:35:09 +0600

    feat: fix tests

--- src/users/users.controller.spec.ts
+++ src/users/users.controller.spec.ts
@@ -1,12 +1,27 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { UsersController } from './users.controller';
+import { UsersService } from './users.service';
 
 describe('UsersController', () => {
   let controller: UsersController;
 
+  const mockUsersService = {
+    fetchUsers: jest.fn(),
+    fetchUserById: jest.fn(),
+    createUser: jest.fn(),
+    updateUserById: jest.fn(),
+    deleteUserById: jest.fn(),
+  };
+
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [UsersController],
+      providers: [
+        {
+          provide: UsersService,
+          useValue: mockUsersService,
+        },
+      ],
     }).compile();
 
     controller = module.get<UsersController>(UsersController);
